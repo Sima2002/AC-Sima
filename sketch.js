98aa619516e4b21a0cf02fe23e384be4071c7525
Sima2002 <sima.alaverdyan.y@tumo.org>
2019-06-26 16:36:45 +0400

    sdgfdhd

--- sketch.js
+++ sketch.js
@@ -1,24 +1,37 @@
 var arr = [];
 
+var smoothMicLevel = 0;
 var n = 0;
+var song;
+var button;
+var amplitude;
 var c = 55;
 var size
-var an = 298.5; //137.5
+var an = 169.5; //179.5 sirun a
 var zoom = 1000;
+
+// function preload() {
+//   song = loadSound("audio/zemfira.mp3");
+// }
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   colorMode(HSB);
   angleMode(DEGREES);
-
+  slider = createSlider(0, 1, 0.5, 0.01);
+  button = createButton("Listen");
+  button.mousePressed(toggleListen);
+  mic = new p5.AudioIn()
+  mic.start();
 
   for (let n = 0; n < 5000; n++) {
 
     var a = n * an;
 
     var r = c * sqrt(n) * n / zoom
-    var col = a % 256;    // n* sin(n)*2/200;
+    var col = a % 256;
     size = cos(n) * 1.5 + 2.5;
     var x = r * sin(a) + width / 2;
     var y = r * cos(a) + height / 2;
@@ -42,6 +55,15 @@ function setup() {
 }
 
 function draw() {
+  micLevel = mic.getLevel();
+  smoothMicLevel = lerp(smoothMicLevel, micLevel, 0.2);
+  zoom += 10000 * smoothMicLevel;
+  console.log(smoothMicLevel)
+  // song.setVolume(slider.value());
+
+  // var vol = amplitude.getLevel();
+  // var diam = map(vol, 0, 0.2, 100, height);
+  //console.log(frameRate);
   background(0);
   for (var i = 0; i < arr.length; i++) {
     var v = arr[i];
@@ -58,6 +80,14 @@ function draw() {
 
 
   if (keyIsPressed) {
+
+
+    // if (frameRate >= 1000) {
+    //   for (let i = 0; i < 10; i++) {
+    //     zoom -= 100;
+
+    //   }
+    // }
     if (key == '+') zoom -= 100;
     else if (key == '-') zoom += 100;
     else if (key == 'r') zoom *= -1;
@@ -67,7 +97,7 @@ function draw() {
 
 
 
-    an = 298.5;
+    an = 169.5;
 
     for (var i = 0; i < arr.length; i++) {
 
@@ -92,9 +122,6 @@ function Character(x, y, col, h) {
   this.target = createVector(x, y)
   this.maxspeed = 13;
   this.maxforce = 15;
-  // this.r= random(20,255);
-  //  this.g= random(20,255);
-  //   this.b= random(20,255);
   this.col = col;
   this.h = h;
 }
@@ -131,7 +158,7 @@ Character.prototype.arrive = function (target) {
   var desired = p5.Vector.sub(target, this.pos);
   var d = desired.mag();
   var speed = this.maxspeed;
-  if (d < 100) {
+  if (d < 80) {
     speed = map(d, 0, 100, 0.2, this.maxspeed)
   }
   desired.setMag(speed);
@@ -157,4 +184,25 @@ Character.prototype.show = function () {
   //strokeWeight(3)
   noStroke();
   ellipse(this.pos.x, this.pos.y, this.h + 3, this.h + 3)
+}
+
+// function togglePlay() {
+//   if (!song.isPlaying()) {
+//     song.loop();
+//     button.html("Pause");
+//   } else {
+//     song.stop();
+//     button.html("Play");
+//   }
+// }
+function toggleListen() {
+  if (getAudioContext().state !== 'running') {
+    getAudioContext().resume();
+    text('listening to audio', width / 2, height / 2);
+    button.html("Stop");
+  } else {
+    text('click Play button to start', width / 2, height / 2);
+
+    button.html("Listen");
+  }
 }
